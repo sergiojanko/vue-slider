aeac8b17e448fa9bc2cb8d9ba0be69be16e2487c
sergiojanko <janko.sergio1998@gmail.com>
2021-07-30 16:08:40 +0200

    render-image

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log(Vue);
+const app = new Vue({
+    el: "#app",
+    data: {
+        images: [
+            "img/image1.jpg",
+            "img/image2.jpg",
+            "img/image3.jpg",
+            "img/image4.jpg",
+        ],
+        currentIndex: 0,
+    },
+    methods: {
+        isActive(i){
+            return this.currentIndex == i ? "active" : "";
+        },
+    }
+});
